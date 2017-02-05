fa0b7bee00252a4e514fa4c8a687988f0fd92d1f
Haley Alexander <hal.00.alexander@gmail.com>
2017-02-05 14:09:30 -0600

    Update MainPokemon.java

--- MainPokemon.java
+++ MainPokemon.java
@@ -25,6 +25,46 @@ public class Main {
 		System.out.println("Ash trains with " + Team[0].getName() + ", " + Team[1].getName() + ", " + Team[2].getName() +", and " + Team[3].getName());
 		System.out.println(Team[0].getName() + " has " + Team[0].getBadges() + " badges because Ash has " + Ash.getBadges() + " badges");
 		
+		
+		//comparing 2 pikachus 
+		Pokemon pika = new Pokemon(8, "Pikachu", 99, "Electric"); 
+		//bad way
+		System.out.println(Team[0] == pika); 
+		//good way
+		System.out.println(Team[0].equals(pika));
+		
+		
+		//type cast a trainer into a pokemon in case you are playing Pokemon Mystery Duegon 
+		//But you shouldn't downcast. It's better for changing customers and employee status
+		//Pokemon Blue = (Pokemon)Ash; 
+		
+		//instanceOf example
+		if (Ash instanceof Trainer){
+			System.out.println("Ash is a trainer");
+		}
+		else{
+			System.out.println("Oh no, Ash got turned into a Pokemon"); 
+		}
+		
+		//interface calling 
+		GoTrain newTrainer = new Trainer(6); 
+		newTrainer.sayTrain();
+		//newTrainer.getBadges(); GoTrain is not a Trainer
+		
+		//polymorphism: 
+		//Trainer is an object 
+		//Trainer is GoTrain 
+		//Pokemon is a Trainer
+		
+		//polymorphism + interfaces -> over-riding and class designing
+		
+		
+		//casting references 
+		//shouldn't down cast, but here it is: Pokemon(Ash) 
+		//this casts the reference of Ash, earlier we tried to turn Ash into a pokemon
+		 
+		
 	}
+
 
 }
